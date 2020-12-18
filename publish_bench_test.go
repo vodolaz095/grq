7b0f50837c8ac1cad7a505cf7a178da9043d6622
Anatolij Ostroumov <ostroumov095@gmail.com>
2020-12-18 21:55:50 +0300

    counting in benchmark

--- publish_bench_test.go
+++ publish_bench_test.go
@@ -18,6 +18,12 @@ func BenchmarkRedisQueue_Publish(b *testing.B) {
 			b.Errorf("%s : while publishing task %v", err, i)
 		}
 	}
+	n, err := publisher.Count()
+	if err != nil {
+		b.Errorf("%s : while counting messages in queue", err)
+	}
+	b.Logf("We managed to publish %v messages", n)
+
 	err = publisher.Purge()
 	if err != nil {
 		b.Errorf("%s : while purging benchmark queue", err)
